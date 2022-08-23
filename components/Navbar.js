4257017a62a50225e45e4c6fb583ebc1e7a1c0a6
Jerry Ye <jerry1ye10@gmail.com>
2022-08-22 23:42:35 -0400

    update countdown

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
+import Countdown from "react-countdown";
 
 export default () => {
   const router = useRouter();
@@ -17,25 +18,38 @@ export default () => {
   }, []);
 
   return (
-    <nav className="flex top-0 inline-block w-screen p-6">
-      {isIndex || (
-        <a href="/" className="raleway text-4xl font-bold mr-6">
-          Founder's Choice
-        </a>
-      )}
-      {(!isIndex && isMobile) || (
-        <>
-          <a
-            href="/ranking"
-            className="raleway text-3xl font-light ml-auto mr-6"
-          >
-            Ranking
+    <>
+      <div class="bg-gray-300">
+        <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8 text-center">
+          <p class="font-medium">
+            {" "}
+            We're releasing our first ranking list soon. Get your rankings in
+            now! Time remaining:{" "}
+            <Countdown date={"2022-09-10T00:00:00"}></Countdown>
+          </p>
+        </div>
+      </div>
+
+      <nav className="flex top-0 inline-block w-screen p-6">
+        {isIndex || (
+          <a href="/" className="raleway text-4xl font-bold mr-6">
+            Founder's Choice
           </a>
-          <a href="/about" className="raleway font-light text-3xl mr-6">
-            About
-          </a>
-        </>
-      )}
-    </nav>
+        )}
+        {(!isIndex && isMobile) || (
+          <>
+            <a
+              href="/ranking"
+              className="raleway text-3xl font-light ml-auto mr-6"
+            >
+              Ranking
+            </a>
+            <a href="/about" className="raleway font-light text-3xl mr-6">
+              About
+            </a>
+          </>
+        )}
+      </nav>
+    </>
   );
 };
