20acc17560cd993d5b33b2bf5323d465d9d8c6a3
Jerry Ye <jerry1ye10@gmail.com>
2023-01-17 11:18:58 -0500

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import Countdown from "react-countdown";
@@ -34,17 +34,32 @@ export default function Navbar() {
     );
   };
 
+  const deadline = new Date("2023-02-08 UTC-4");
+
   useEffect(() => {
     setIsMobile(window.innerWidth < 768);
     const handleResize = () => setIsMobile(window.innerWidth <= 768);
+    const current = new Date();
+    const difference = dateDiffInDays(current, deadline);
+    setCountDown(difference);
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const deadline = new Date("2022-09-10 UTC-4");
-
   return (
     <>
+      {isIndex && (
+        <div class="bg-gray-300">
+          <div class="max-w-7xl mx-auto py-5 px-3 sm:px-6 lg:px-8 text-center">
+            <p class="font-medium text-2xl">
+              {" "}
+              We're releasing our first ranking list soon. Get your rankings in
+              now! Time remaining: {countdown} days
+            </p>
+          </div>
+        </div>
+      )}
       <nav className="flex top-0 inline-block w-screen p-6">
         {isIndex || (
           <a href="/" className="raleway sm:text-4xl text-xl font-bold mr-6">
